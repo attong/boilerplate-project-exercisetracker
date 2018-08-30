ce8053f22a71c83f4d48033da4361bae298493f1
attong <attong@berkeley.edu>
2018-08-29 20:57:20 -0700

    adding user and verifying users to database implemented

--- server.js
+++ server.js
@@ -5,19 +5,44 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
+//mongoose.connect(process.env.MLAB_URI || 'mongodb://localhost/exercise-track' )
 
 app.use(cors())
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
+mongoose.connect('mongodb://localhost:27017/exercisetracker');
 
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Define schema
+var Schema = mongoose.Schema;
+var exerciseSchema = new Schema({
+  user: {type: String, unique: true},
+  excercises: {type: Array}//initialize as empty array, update as we add more
+});
+var users = mongoose.model("user", exerciseSchema);
+
+
+app.post('/api/exercise/new-user', (req,res)=>{
+	if (!req.body) return res.sendStatus(400);
+	// verify that parsing works
+	users.findOne({user: req.body.username}, (err,data)=>{
+		if (err){
+			throw err;
+		} else if (data == null){
+			var newUser = new users({user: req.body.username, excercises:[]});
+			newUser.save();
+			res.json({success: req.body.username + " added to database"});
+		} else{
+			res.json({err: req.body.username + " is already in the system"});
+		}
+	});
+})
 
 // Not found middleware
 app.use((req, res, next) => {
